e131e2c50122c318288fb0cf8d072bfa78b0e7c0
Your Name <oemarsha@uncg.edu>
2016-04-12 15:42:17 -0400

    Generic outline for store mode

--- FutureList/src/statGen.java
+++ FutureList/src/statGen.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Map.Entry;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,16 +12,40 @@ import java.util.*;
  */
 public class statGen {
     public static double meanPrice(ArrayList serries){
+        Purchase[] sarr = serries.toArray();
         double sum = 0;
-        for (int i = 0; i < serries.size(); i++) {
-            sum += serries.get(i).getitemPrice();
+        for (int i = 0; i < sarr.length; i++) {
+            sum += sarr[i].getItemPrice();
         }
         double mean = sum / serries.size();
         return mean;
     }
     
     public static String modeStore(ArrayList serries){
-       throw new UnsupportedOperationException();
+        Purchase[] sarr =serries.toArray();
+        int max=1, count=0;
+        String tempstr;
+        List<String> theList = Arrays.asList(sarr);
+        HashMap<String,Integer> serhm =new HashMap<String,Integer>();
+        for (int i = 0; i < sarr.length(); i++) {
+            tempstr = sarr[i];
+            if (serhm.containsKey(tempstr)) {
+                serhm.put(tempstr, serhm.get(tempstr)+1);
+            }
+            else{
+                serhm.put(tempstr,1);
+            }
+            if (max < count){
+                max = count;
+            }
+        }
+        for (Entry<String, Integer> entry: serhm.entrySet()) {
+            if (max == entry.getValue()){
+                return entry.getKey();
+            }
+            
+        }
+        return null;
     }
     public static String modeCatagory(ArrayList serries){
         throw new UnsupportedOperationException();
