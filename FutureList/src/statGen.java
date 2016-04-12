09d60ffb644c58055a2bfae8a381e024adbfca55
Your Name <oemarsha@uncg.edu>
2016-04-12 14:21:47 -0400

    finished mean

--- FutureList/src/statGen.java
+++ FutureList/src/statGen.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +10,25 @@
  * @author Ochaun Marshall
  */
 public class statGen {
+    public static double meanPrice(ArrayList serries){
+        double sum = 0;
+        for (int i = 0; i < serries.size(); i++) {
+            sum += serries.get(i).getitemPrice();
+        }
+        double mean = sum / serries.size();
+        return mean;
+    }
     
+    public static String modeStore(ArrayList serries){
+       throw new UnsupportedOperationException();
+    }
+    public static String modeCatagory(ArrayList serries){
+        throw new UnsupportedOperationException();
+    }
+    public static String modeItem(ArrayList serries){
+        throw new UnsupportedOperationException();
+    }
+    public static ArrayList predict(ArrayList serries){
+        throw new UnsupportedOperationException();
+    }
 }
